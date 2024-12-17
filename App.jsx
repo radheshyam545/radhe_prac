4faac62a26cb066e4fe8227e95a6f36dbacb8732
Himanshujalan <48562116+Himanshujalan@users.noreply.github.com>
2024-12-17 18:55:14 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,21 @@
+import { ParallaxProvider } from "react-scroll-parallax";
+import ParallaxEffect from "./components/parallax/parallax";
+import HomePage from "./home";
+
+export default function App() {
+  return (
+    <>
+    {/* <ParallaxProvider> */}
+    <HomePage/> 
+    {/* </ParallaxProvider> */}
+    {/* <ParallaxEffect/> */}
+    {/* <div className="font-saveya text-lg">
+  This text uses the Saveya font.
+</div>
+<div className="font-editorial text-lg">
+  This text uses the Editorial font.
+</div> */}
+
+    </>
+  )
+}
